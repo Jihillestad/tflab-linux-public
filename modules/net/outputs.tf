b422a613f8b24d0c2c93f2a2fa369839bc9dd12a
Jann Idar Hillestad <64490910+Jihillestad@users.noreply.github.com>
2025-10-08 22:06:04 +0200

    feat: add outputs for NAT Gateway and Application Gateway

--- modules/net/outputs.tf
+++ modules/net/outputs.tf
@@ -27,3 +27,28 @@ output "bastion_public_ip" {
   description = "The public IP address of the Bastion host"
   value       = azurerm_public_ip.bastion_pip.ip_address
 }
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = azurerm_nat_gateway.nat_gateway.id
+}
+
+output "nat_gateway_public_ip" {
+  description = "The public IP address of the NAT Gateway"
+  value       = azurerm_public_ip.nat_gateway_pip.ip_address
+}
+
+output "appgw_id" {
+  description = "The ID of the Application Gateway"
+  value       = azurerm_application_gateway.appgw.id
+}
+
+output "appgw_public_ip" {
+  description = "The public IP address of the Application Gateway"
+  value       = azurerm_public_ip.appgw_pip.ip_address
+}
+
+output "appgw_backend_pool_id" {
+  description = "The ID of the Application Gateway backend pool"
+  value       = tolist(azurerm_application_gateway.appgw.backend_address_pool)[0].id
+}
